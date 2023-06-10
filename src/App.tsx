d3b3077d6353d0be4863d94cd370d1bdccabb183
Ono　Keita <oh.kitbsk.1028@icloud.com>
2023-06-10 12:29:11 +0000

    feat: 見た目作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
 import "./App.css";
+import arrow from "./assets/arrow.svg";
 
 function App() {
-  return <div />;
+  function translate(): void {
+    // eslint-disable-next-line no-console
+    console.log("Hello world!");
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="titleBox">
+          <p className="titleText">翻訳サイト</p>
+        </div>
+        <div className="inputTextBox">
+          <input
+            type="text"
+            className="inputText"
+            placeholder="翻訳したい日本語を入力"
+          />
+          <div className="inputTextUnderline" />
+        </div>
+        <div className="buttonBox">
+          <button type="submit" className="translateButton" onClick={translate}>
+            翻訳
+          </button>
+        </div>
+        <div className="arrowBox">
+          <img src={arrow} alt="arrow" />
+        </div>
+        <div className="translatedTextBox">
+          <p className="translatedText">
+            If the exact weight given is unavailable, then the following rule is
+            used to determine the weight actually rendered
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
